2f9c8e4dbb489c70bb84e44aa24bfb5235a01511
BenV42 <134002845+BenV42@users.noreply.github.com>
2023-07-09 15:52:47 -0400

    Demos added / get-ipreport

---
+++ powershell/get-ipreport.ps1
@@ -0,0 +1,13 @@
+function get-ipreport {
+get-ciminstance win32_networkadapterconfiguration | 
+where { $_.IPEnabled -eq $True } | 
+sort Description, Index, IPAddress, IPSubnet, DNSHostName, DNSDomain, DNSServerSearchOrder, DHCPEnabled |
+format-table -Autosize -Wrap | out-string -width 500
+}
+
+#Trying to figure out formatting of table
+
+#get-ciminstance win32_networkadapterconfiguration | 
+#where { $_.IPEnabled -eq $True } |
+#new-object -typename psobject -property @{Description=$_.Description; Index=$_.Index; IPAddress=$_.IPAddress; IPSubnet=$_.IPSubnet; #DNSHostName=$_.DNSHostName; DNSDomain=$_.DNSDomain; DNSServerSearchOrder=$_.DNSServerSearchOrder; DHCPEnabled=$_.DHCPEnabled | 
+#format-table -Autosize | format-list
