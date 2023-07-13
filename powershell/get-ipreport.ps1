2ce1e5217fe6e9ab82c5ccad2b8a3a5f027085f2
BenV42 <134002845+BenV42@users.noreply.github.com>
2023-07-13 16:25:13 -0400

    Update get-ipreport.ps1

--- powershell/get-ipreport.ps1
+++ powershell/get-ipreport.ps1
@@ -1,13 +1,11 @@
+#Network Table formatted into a table based on IPEnabled
+
 function get-ipreport {
 get-ciminstance win32_networkadapterconfiguration | 
 where { $_.IPEnabled -eq $True } | 
-sort Description, Index, IPAddress, IPSubnet, DNSHostName, DNSDomain, DNSServerSearchOrder, DHCPEnabled |
-format-table -Autosize -Wrap | out-string -width 500
+sort Index |
+format-table -Autosize Description, Index, IPAddress, IPSubnet, DNSHostName,
+DNSDomain, DNSServerSearchOrder, DHCPEnabled
 }
 
-#Trying to figure out formatting of table
 
-#get-ciminstance win32_networkadapterconfiguration | 
-#where { $_.IPEnabled -eq $True } |
-#new-object -typename psobject -property @{Description=$_.Description; Index=$_.Index; IPAddress=$_.IPAddress; IPSubnet=$_.IPSubnet; #DNSHostName=$_.DNSHostName; DNSDomain=$_.DNSDomain; DNSServerSearchOrder=$_.DNSServerSearchOrder; DHCPEnabled=$_.DHCPEnabled | 
-#format-table -Autosize | 
