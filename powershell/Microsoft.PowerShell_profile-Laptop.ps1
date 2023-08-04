07c153cd50614262690c3046aab67b9e237c04bd
BenV42 <134002845+BenV42@users.noreply.github.com>
2023-08-03 20:51:02 -0400

    changed profiles

--- powershell/Microsoft.PowerShell_profile-Laptop.ps1
+++ powershell/Microsoft.PowerShell_profile-Laptop.ps1
@@ -1,8 +1,3 @@
 $env:PATH += ";C:\Users\benve\Documents\Github\COMP2101\powershell"
 new-item -path alias:np -value notepad | Out-Null
-function welcomemsg {
-$time = get-date -format 'HH:mm ddd d MMM yyyy'
-write-output "Welcome to Powershell $env:username"
-write-output "Have a Crazy Time at $time."
-}
 welcomemsg
--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,20 +1,3 @@
 $env:PATH += ";C:\Users\administrator\Documents\GitHub\COMP2101\powershell"
 new-item -path alias:np -value notepad | Out-Null
-
-function welcomemsg {
-$time = get-date -format 'HH:mm on ddd d MMM in yyyy'
-write-output "Welcome to Powershell $env:username"
-write-output "Have a Crazy Time at $time."
-}
-
-function get-cpuinfo {
-	get-ciminstance cim_processor | format-list DeviceID, Description,
- 	CurrentClockspeed, MaxClockSpeed, Manufacturer, NumberOfCores
-}
-
-function get-mydisks {
-	get-disk | format-table DiskNumber, Manufacturer, Model, 
-	SerialNumber, FirmwareVersion, Size
-} 
-
 welcomemsg
